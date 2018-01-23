f90c426936ec0bc365b9ebe764b736cb41bb41fd
MarkKlik <markklik@gmail.com>
2018-01-23 09:13:33 +0100

    Reverse size argument in ZSTD compressor call

--- tests/fstlibtests/factors.cpp
+++ tests/fstlibtests/factors.cpp
@@ -24,11 +24,28 @@ class FactorTest : public ::testing::Test
 protected:
 	FilePath testDataDir;
 	std::string filePath;
+  IColumnFactory* columnFactory;
 
-	virtual void SetUp()
-	{
-		filePath = GetFilePath("2.fst");
-	}
+  IFstTable* tableReader;
+  IStringArray* selectedCols;
+  std::vector<int> keyIndex;
+  StringArray* columnSelection = nullptr;
+
+  virtual void SetUp()
+  {
+    filePath = GetFilePath("2.fst");
+    tableReader = new FstTable();
+    selectedCols = new StringArray();
+    columnFactory = new ColumnFactory();
+  }
+
+  virtual void TearDown()
+  {
+    delete tableReader;
+    delete selectedCols;
+    delete columnFactory;
+    delete columnSelection;
+  }
 };
 
 
@@ -52,3 +69,43 @@ TEST_F(FactorTest, NaLevels)
 	ReadWriteTester::WriteReadSingleColumns(fstTable, filePath, 0);
 }
 
+
+TEST_F(FactorTest, MediumLevels)
+{
+  const int nrOfRows = 1;
+  FstTable fstTable(nrOfRows);
+  fstTable.InitTable(1, nrOfRows);
+
+  vector<std::string> colNames{ "Factor" };
+  fstTable.SetColumnNames(colNames);
+
+  // Add factor column
+  FactorVectorAdapter factorVec(nrOfRows);
+  IntSeq(factorVec.LevelData(), nrOfRows, 1);
+  StringColumn* levels = factorVec.DataPtr()->Levels();
+
+  int nr_of_levels = 128;
+  levels->AllocateVec(nr_of_levels);
+
+  std::vector<std::string> levelVec = *(levels->StrVector()->StrVec());
+  for (int pos = 0; pos < nr_of_levels; pos++)
+  {
+    levelVec[pos] = to_string(pos);
+  }
+
+  fstTable.SetFactorColumn(&factorVec, 0);
+
+  FstStore fstStore(GetFilePath("medium_factor2.fst"));
+  fstStore.fstWrite(fstTable, 50);
+
+  ReadWriteTester::WriteReadSingleColumns(fstTable, filePath, 50);
+}
+
+
+TEST_F(FactorTest, FactorRead)
+{
+  FstStore fstStore(GetFilePath("medium_factor2.fst"));
+
+  // Read fst file
+  fstStore.fstRead(*tableReader, columnSelection, 1, -1, columnFactory, keyIndex, selectedCols);
+}
