4efeeabf334a5316a221b23a5625555147d71255
markklik <markklik@gmail.com>
2020-02-04 21:15:08 +0100

    Add quicksort, merge sort and radix sort algorithms

---
+++ lib/fst/sort/sort.h
@@ -0,0 +1,35 @@
+/*
+  fstlib - A C++ library for ultra fast storage and retrieval of datasets
+
+  Copyright (C) 2017-present, Mark AJ Klik
+
+  This file is part of fstlib.
+
+  fstlib is free software: you can redistribute it and/or modify it under the
+  terms of the GNU Affero General Public License version 3 as published by the
+  Free Software Foundation.
+
+  fstlib is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+  details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with fstlib. If not, see <http://www.gnu.org/licenses/>.
+
+  You can contact the author at:
+  - fstlib source repository : https://github.com/fstpackage/fstlib
+*/
+
+
+#ifndef FST_SORT_H
+#define FST_SORT_H
+
+
+void fst_radix_sort(int* vec, int length, int* buffer);
+
+void fst_merge_sort(const int* left_p, const int* right_p, int length_left, int length_right, int* res_p);
+
+void fst_quick_sort(int* vec, int length, int pivot);
+
+#endif  // FST_SORT_H
